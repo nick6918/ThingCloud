be418e823e4ea40a6a1337b09497cc25ffe67376
weifanding <weifanding@yiqiding.com>
2015-07-11 16:15:43 +0800

    modified:   picture.py

--- ThingCloud/TCD_lib/picture.py
+++ ThingCloud/TCD_lib/picture.py
@@ -1,5 +1,5 @@
 from TCD_lib import settings
-from TCD_lib.upyun import UpYun
+from TCD_lib.upyun.upyun import UpYun
 # from multiprocessing import Process
 import logging
 logger= logging.getLogger('appserver')
