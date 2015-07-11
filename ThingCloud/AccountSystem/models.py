467956f4c475e5b08332f69a78d9f5ffac8c9cb0
weifanding <weifanding@yiqiding.com>
2015-07-11 13:15:06 +0800

    modified:   AccountSystem/models.py

--- ThingCloud/AccountSystem/models.py
+++ ThingCloud/AccountSystem/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
 	uid = models.AutoField(primary_key = True)
 	gid = models.IntegerField(max_length = 10)
 	nickname = models.CharField(max_length=200)
-	gender = models.IntergerField()
+	gender = models.IntegerField()
 	birthday = models.CharField(max_length = 50)
 	register = models.DateTimeField()
 	lastLogin = models.DateTimeField()
