f2746e27704e2b8f93c4ab0c5915a1f05b920a16
weifanding <weifanding@yiqiding.com>
2015-07-11 17:33:22 +0800

    modified:   picture.py

--- ThingCloud/TCD_lib/picture.py
+++ ThingCloud/TCD_lib/picture.py
@@ -7,7 +7,7 @@ logger= logging.getLogger('appserver')
 
 class Picture(object):
 	def __init__(self, _conn=None):
-		super(PictureModel, self).__init__()
+		super(Picture, self).__init__()
 		self.cursor = _conn
 	def uploadPicture(self,path, data, callback=None):
 		upyun = UpYun(settings.image_bucket, settings.image_user, settings.image_password,timeout=4)
