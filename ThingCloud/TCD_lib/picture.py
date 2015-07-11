e5a98f3c1ae79813959cf6615b84e645953e8c21
weifanding <weifanding@yiqiding.com>
2015-07-11 15:43:41 +0800

    new file:   ../TCD_lib/compat.py 	new file:   ../TCD_lib/picture.py 	new file:   ../TCD_lib/settings.py

---
+++ ThingCloud/TCD_lib/picture.py
@@ -0,0 +1,15 @@
+from TCD_libs import settings
+from TCD_libs.upyun import UpYun
+# from multiprocessing import Process
+import logging
+logger= logging.getLogger('appserver')
+
+
+class Picture(object):
+	def __init__(self, _conn=None):
+		super(PictureModel, self).__init__()
+		self.cursor = _conn
+	def uploadPicture(self,path, data, callback=None):
+		upyun = UpYun(settings.image_bucket, settings.image_user, settings.image_password,timeout=4)
+		result=upyun.put(path, data, True)
+		return result
---
+++ ThingCloud/TCD_lib/settings.py
@@ -0,0 +1,5 @@
+# UPYUN SETTING
+image_bucket= "thingcloud-master"
+image_user= "nickwei"
+image_password= "nick885677"
+
