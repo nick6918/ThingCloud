c4739f7eb2602c2ef0660f34a0ed437d5909de9a
fwei <fwei@microstrategy.com>
2016-03-08 20:11:07 +0800

    password change

--- ThingCloud/TCD_lib/settings.py
+++ ThingCloud/TCD_lib/settings.py
@@ -1,6 +1,6 @@
 # UPYUN SETTING
 image_bucket= "thingcloud-master"
-image_user= "nickwei"
-image_password= "thingcloud1234"
+image_user= "nicadmin"
+image_password= "nick885677"
 
 UPYUNURL = "thingcloud-master.b0.upaiyun.com"
