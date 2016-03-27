4a681b5e80087eb53d21f879a579fd981ffb4799
fwei <fwei@microstrategy.com>
2016-03-27 17:49:26 +0800

    vip vip add middle state

--- ThingCloud/VIPSystem/views.py
+++ ThingCloud/VIPSystem/views.py
@@ -20,11 +20,15 @@ def getFee(month, level):
 def vip(request):
     _user = request.user
     if not _user["vip"]:
-        return Jsonify({"status":False, "error":"1501", "error_message":"用户还不是会员, 请先加入会员。"})
+        pOrder = VIPOrder.objects.filter(user_id=_user['uid']).filter(state=2)
+        if pOrder:
+            return Jsonify({"status":False, "error":"1501", "error_message":"用户还不是会员, 请先加入会员。", "processing":1})
+        else:
+            return Jsonify({"status":False, "error":"1501", "error_message":"用户还不是会员, 请先加入会员。", "processing":0})
     _vip = VIP.objects.filter(vid=_user["vip"])
     if _vip:
         _vip = _vip[0]
-        return Jsonify({"status":True, "error":"", "error_message":"", "vip":dictPolish(model_to_dict(_vip)), "user":_user})
+        return Jsonify({"status":True, "error":"", "error_message":"", "processing":0, "vip":dictPolish(model_to_dict(_vip)), "user":_user})
     else:
         return Jsonify({"status":False, "error":"1501", "error_message":"用户还不是会员, 请先加入会员。"})
 
