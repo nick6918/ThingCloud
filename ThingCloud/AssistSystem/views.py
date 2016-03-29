c6480398d74bd97460be5bcb0d90b08fc8feb06f
fwei <fwei@microstrategy.com>
2016-03-29 21:27:27 +0800

    discount polish

--- ThingCloud/AssistSystem/views.py
+++ ThingCloud/AssistSystem/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from models import Feedback
+from models import Feedback, Discount
 from TCD_lib.utils import Jsonify
 from TCD_lib.security import UserAuthorization
 from django.forms.models import model_to_dict
