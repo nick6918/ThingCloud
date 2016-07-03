8ef0b7421ba2691881425cf8fb5f6a3aee593ab1
Fanding WEI <Fanding WEI>
2016-07-03 15:19:28 +0800

    let's go

--- ThingCloud/TCD_lib/utils.py
+++ ThingCloud/TCD_lib/utils.py
@@ -44,7 +44,7 @@ def generateRandomString(size):
         result += ALPHABET[index]
     return result
 
-def md5(m2):
+def md5(src):
     m2 = hashlib.md5()
     m2.update(src)
     return m2.hexdigest()
