6fdcfa2505e72692389edad986cc4f8472722907
Fanding WEI <Fanding WEI>
2016-07-05 21:14:24 +0800

    0.1

--- ThingCloud/VIPSystem/views.py
+++ ThingCloud/VIPSystem/views.py
@@ -26,7 +26,7 @@ def vip(request):
     _vip = VIP.objects.filter(vid=_user["vip"])
     if _vip:
         _vip = _vip[0]
-        return Jsonify({"status":True, "state":True, error":"", "error_message":"", "processing":orderstate, "vip":dictPolish(model_to_dict(_vip)), "user":_user})
+        return Jsonify({"status":True, "state":True, "error":"", "error_message":"", "processing":orderstate, "vip":dictPolish(model_to_dict(_vip)), "user":_user})
     else:
         return Jsonify({"status":False, "state":False, "error":"1501", "error_message":"用户还不是会员, 请先加入会员。", "processing":orderstate})
 
