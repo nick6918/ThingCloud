73a16583ef07b16cbee43ce6a40a403564543268
nick6918 <nick6918@gmail.com>
2016-07-25 17:23:07 +0800

    debug

--- ThingCloud/CloudList/models.py
+++ ThingCloud/CloudList/models.py
@@ -101,6 +101,4 @@ class Thing(models.Model):
 				result['avatarurl'] = PICURL+"thing/"+str(result['tid'])+".png"
 			else:
 				result['avatarurl'] = PICURL+"thing/default.png"
-		for item in result.keys():
-			logger.debug((item, type(result[item])))
 		return result
