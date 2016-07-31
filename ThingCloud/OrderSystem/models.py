cce707cb82d345ec6cf2709060aadf057b60ebf2
nick6918 <nick6918@gmail.com>
2016-07-31 15:41:23 +0800

    fix bug

--- ThingCloud/OrderSystem/models.py
+++ ThingCloud/OrderSystem/models.py
@@ -118,14 +118,17 @@ class Order(models.Model):
 
     def flushInfo(self):
         infoList = html_head + generateInfo(self, self.state) + html_tail
-        fp = open("./htmltext/"+str(self.oid)+".html", "w+")
-        fp.write(infoList)
-        fp.close()
+        # fp = open("./htmltext/"+str(self.oid)+".html", "w+")
+        # fp.write(infoList.encode('UTF-8'))
+        # fp.close()
+        # status = True
+        status = Files().uploadFiles("htmltext/"+ str(self.oid) + ".html", infoList)
         return status
 
     def toDict(self):
         _order = model_to_dict(self)
-        _order["texturl"] = "testapi.thingcloud.net:8001/htmltext/"+ str(self.oid) + ".html"
+        #_order["texturl"] = "testapi.thingcloud.net:8001/htmltext/"+ str(self.oid) + ".html"
+        _order["texturl"] = "staticimage.thingcloud.net/htmltext/" + str(self.oid) + ".html"
         _order["total_units"] = self.getTotalUnits()
         return dictPolish(_order)
 
