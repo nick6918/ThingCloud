95e0cfb458bd90aad29585d3a3a92c5922e6ebbc
nick6918 <nick6918@gmail.com>
2016-08-03 14:51:08 +0800

    debug

--- ThingCloud/OrderSystem/models.py
+++ ThingCloud/OrderSystem/models.py
@@ -8,82 +8,12 @@ from django.forms.models import model_to_dict
 from TCD_lib.utils import dictPolish
 from TCD_lib.picture import Files
 from TCD_lib.settings import PICURL, UPYUNURL
+from TCD_lib.business import generateInfo, html_head, html_tail
 
 FILEPATH = UPYUNURL+u"/orderfiles/"
 
 import logging
 logger = logging.getLogger('appserver')
-
-html_head = u"""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>订单详情</title>
-    <style>
-        .info {
-            color: blue;
-            text-decoration: underline;
-        }
-    </style>
-</head>
-<body>
-
-    """
-html_tail =u"""
-
-</body>
-</html>
-"""
-def generateInfo(order, state):
-    typeid = order.typeid
-    html_text = u""
-    if state < 2:
-        html_text += u"  <p>系统正在处理您的订单, 请稍候查看。</p>\n"
-    elif state == 2:
-        html_text += u"  <p>您的订单已创建, 正由<span class=info>"+order.addr.community_belong.wh_in.name+u"</span>处理中。</p>\n"
-        if typeid == 0:
-            html_text += u"  <p>正在等待快递员出单, 预计30分钟内前往<span class=info>"+order.addr.name+u"</span>取件。</p>\n"
-            html_text += u"  <p>请准备好您要存入邻仓的物品， 快递员会上门取件并向您出具存货确认单。</p>\n"
-        else:
-            html_text += u"  <p>正在等待快递员出单, 预计30分钟内即将您的订单送至<span class=info>"+order.addr.name+u"。</span></p>\n"
-    elif state==3:
-        html_text = html_text + generateInfo(order, 2)
-        if typeid == 0:
-            try:
-                html_text += u"  <p>快递员<span class=info>"+order.courier.worker.name+u"("+order.courier.worker.phone+u")</span>已出单, 预计20分钟内将您的订单送至<span class=info>"+order.addr.name+u"</span>。</p>\n"
-            except Exception,e :
-                html_text += u"  <p>快递员已出单, 预计20分钟内前往<span class=info>"+order.addr.name+u"</span>取件。</p>\n"              
-        else:
-            try:
-                html_text += u"  <p>快递员<span class=info>"+order.courier.worker.name+u"("+order.courier.worker.phone+u")</span>已出单, 预计20分钟内将您的订单送至<span class=info>"+order.addr.name+u"</span>。</p>\n"
-            except Exception,e :
-                html_text += u"  <p>快递员已出单, 预计20分钟内将您的订单送至<span class=info>"+order.addr.name+u"</span>。</p>\n"              
-    elif state==4:
-        html_text = html_text + generateInfo(order, 3)
-        html_text += u"  <p>快递员已取到货品, 预计1小时内送货入库。货品入库后, 您可在订单页面或者邻仓主页查看所有您存入的商品。</p>\n"
-    elif state==5:
-        if typeid == 0:
-            html_text = html_text + generateInfo(order, 4)
-            html_text += u"  <p>快递员已上传存货单, <a href="+PICURL+str(order.oid)+ u".png >点此查看</a>。</p>\n"
-            html_text += u"  <p>存货成功，快递员已上传全部商品， 请前往主页确认。如有疑问, 请联系客服。</p>\n"
-        else:
-            html_text = html_text + generateInfo(order, 3)
-            html_text += u"  <p>快递员已上传存货单, <a href="+PICURL+str(order.oid)+ u".png >点此查看</a>。</p>\n"
-            html_text += u"  <p>取件成功， 快递员已将商品送至指定地址， 请您确认。如有疑问, 请联系客服。</p>\n"
-    elif state == 7 or state == 8:
-        html_text = html_text + generateInfo(order, 3)
-        html_text += u"  <p>您的订单已取消。如有疑问, 请联系客服。</p>\n"
-    elif state == 9:
-        html_text = html_text + generateInfo(order, 5)
-        html_text += u"  <p>系统已收到您的申诉请求, 我们将于1天内与您取得联系。</p>\n"
-    elif state == 6:
-        html_text = html_text + generateInfo(order, 5)
-        html_text += u"  <p>您已确认订单无误， 订单完成。谢谢您的使用。</p>\n"
-    else:
-        html_text = html_text + generateInfo(order, 3)
-        html_text += u"  <p>您的订单已取消。如有疑问, 请联系客服。</p>\n"
-    return html_text
 
 # Create your models here.
 class Courier(models.Model):
--- ThingCloud/TCD_lib/business.py
+++ ThingCloud/TCD_lib/business.py
@@ -1,82 +1,75 @@
-from VIPSystem.models import VIP, VIPPackage
-from AccountSystem.models import User
-from datetime import datetime, timedelta
-from django.utils import timezone
+# -*- coding: utf-8 -*-
 
-def changePackage(vip):
-	#No need to flush
-	vip.headPackage.days = 0
-	vip.headPackage.save()
-	if vip.headPackage.nextPackage:
-		vip.headPackage.nextPackage.start_date = timezone.now()
-		vip.headPackage.nextPackage.save()
-	vip.headPackage = vip.headPackage.nextPackage
-	vip.save()
-	return vip
-
-def flushVip(vip):
-	if vip:
-		total_timedelta = timezone.now() - vip.headPackage.start_date
-		difference = total_timedelta - timedelta(vip.headPackage.days)
-		while difference >= timedelta(0):
-			total_timedelta = difference
-			vip = changePackage(vip)	
-			difference = total_timedelta - timedelta(vip.headPackage.days)
-		vip.headPackage.days = vip.headPackage.days - total_timedelta.days
-		vip.headPackage.save()
-		return vip
-	else:
-		return vip
-
-def addNewHeadPackage(vip, newPackage):
-	vip = flushVip(vip)
-	vip.headPackage.days = vip.headPackage.days - (timezone.now() - vip.headPackage.start_date).days
-	vip.headPackage.save()
-	newPackage.nextPackage = vip.headPackage
-	newPackage.start_date = timezone.now()
-	newPackage.save()
-	vip.headPackage = newPackage
-	vip.save()
-	return vip
-
-def addNewPackage(month, typeid, vip=None, user=None):
-	if type(user) == dict:
-		user = User.objects.filter(uid=user['uid'])[0]
-	vip = flushVip(vip)
-	if vip:
-		currentPackage = vip.headPackage
-		if currentPackage.level == typeid:
-			currentPackage.days = currentPackage.days + month*31
-			currentPackage.save()
-		else:		
-			if currentPackage.level < typeid:
-				newPackage = VIPPackage(start_date = None, days = month*31, level=typeid, nextPackage=None)
-				newPackage.save()
-				vip = addNewHeadPackage(vip, newPackage)
-			else:
-				fatherPackage = None
-				while currentPackage.level > typeid:
-					fatherPackage = currentPackage
-					currentPackage = currentPackage.nextPackage
-				if currentPackage.level == typeid:
-					currentPackage.days = currentPackage.days + month * 31
-					currentPackage.save()
-				else:
-					newPackage = VIPPackage(start_date = None, days = month*31, level=typeid, nextPackage=currentPackage)
-					newPackage.save()
-					fatherPackage.nextPackage = newPackage
-					fatherPackage.save()
-		return vip				
-	else:
-		vip = VIP()
-		vip.save()
-		newPackage = VIPPackage(start_date = timezone.now(), days = month*31, level=typeid)
-		newPackage.save()
-		vip.headPackage = newPackage
-		vip.save()
-		user.vip = vip
-		user.save()
-		return vip
+from TCD_lib.settings import PICURL, UPYUNURL
 
 
-	
+html_head = u"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>订单详情</title>
+    <style>
+        .info {
+            color: blue;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+
+    """
+html_tail =u"""
+
+</body>
+</html>
+"""
+def generateInfo(order, state):
+    typeid = order.typeid
+    html_text = u""
+    if state < 2 or state >= 12:
+        html_text += u"  <p>系统正在处理您的订单, 请稍候查看。</p>\n"
+    elif state == 2:
+        html_text += u"  <p>您的订单已创建, 正由<span class=info>"+order.addr.community_belong.wh_in.name+u"</span>处理中。</p>\n"
+        if typeid == 0:
+            html_text += u"  <p>正在等待快递员出单, 预计30分钟内前往<span class=info>"+order.addr.name+u"</span>取件。</p>\n"
+            html_text += u"  <p>请准备好您要存入邻仓的物品， 快递员会上门取件并向您出具存货确认单。</p>\n"
+        else:
+            html_text += u"  <p>正在等待快递员出单, 预计30分钟内即将您的订单送至<span class=info>"+order.addr.name+u"。</span></p>\n"
+    elif state==3:
+        html_text = html_text + generateInfo(order, 2)
+        if typeid == 0:
+            try:
+                html_text += u"  <p>快递员<span class=info>"+order.courier.worker.name+u"("+order.courier.worker.phone+u")</span>已出单, 预计20分钟内将您的订单送至<span class=info>"+order.addr.name+u"</span>。</p>\n"
+            except Exception,e :
+                html_text += u"  <p>快递员已出单, 预计20分钟内前往<span class=info>"+order.addr.name+u"</span>取件。</p>\n"              
+        else:
+            try:
+                html_text += u"  <p>快递员<span class=info>"+order.courier.worker.name+u"("+order.courier.worker.phone+u")</span>已出单, 预计20分钟内将您的订单送至<span class=info>"+order.addr.name+u"</span>。</p>\n"
+            except Exception,e :
+                html_text += u"  <p>快递员已出单, 预计20分钟内将您的订单送至<span class=info>"+order.addr.name+u"</span>。</p>\n"              
+    elif state==4:
+        html_text = html_text + generateInfo(order, 3)
+        html_text += u"  <p>快递员已取到货品, 预计1小时内送货入库。货品入库后, 您可在订单页面或者邻仓主页查看所有您存入的商品。</p>\n"
+    elif state==5:
+        if typeid == 0:
+            html_text = html_text + generateInfo(order, 4)
+            html_text += u"  <p>快递员已上传存货单, <a href="+PICURL+str(order.oid)+ u".png >点此查看</a>。</p>\n"
+            html_text += u"  <p>存货成功，快递员已上传全部商品， 请前往主页确认。如有疑问, 请联系客服。</p>\n"
+        else:
+            html_text = html_text + generateInfo(order, 3)
+            html_text += u"  <p>快递员已上传存货单, <a href="+PICURL+str(order.oid)+ u".png >点此查看</a>。</p>\n"
+            html_text += u"  <p>取件成功， 快递员已将商品送至指定地址， 请您确认。如有疑问, 请联系客服。</p>\n"
+    elif state == 7 or state == 8:
+        html_text = html_text + generateInfo(order, 3)
+        html_text += u"  <p>您的订单已取消。如有疑问, 请联系客服。</p>\n"
+    elif state == 9:
+        html_text = html_text + generateInfo(order, 5)
+        html_text += u"  <p>系统已收到您的申诉请求, 我们将于1天内与您取得联系。</p>\n"
+    elif state == 6:
+        html_text = html_text + generateInfo(order, 5)
+        html_text += u"  <p>您已确认订单无误， 订单完成。谢谢您的使用。</p>\n"
+    else:
+        html_text = html_text + generateInfo(order, 3)
+        html_text += u"  <p>您的订单已取消。如有疑问, 请联系客服。</p>\n"
+    return html_text
